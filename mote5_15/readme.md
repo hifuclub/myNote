e028ad6939c0159e911cdea9a81276d911c29fee
hifuclub <hifuclub@outlook.com>
2019-06-27 18:25:33 +0800

    0515
    Signed-off-by: hifuclub <hifuclub@outlook.com>

---
+++ mote5_15/readme.md
@@ -0,0 +1,15 @@
+下载后打开luaTest文件夹下XLua.exe文件,就可以开始调试(安卓版下载安装apk文件)
+
+在下面lua服务端lua code输入的lua代码会在unity中执行
+
+如果为text变量赋值,如输入text = 'I like study',会改变之中的显示
+
+源代码在MyText.cs,其他lua学习笔记将会在之后上传逐步补全
+
+----------------------------------
+
+lua知识点:http://tlmc.top:8090/Knowledge_Points/lua_point
+
+lua服务端:http://tlmc.top:8090/ipget/07/
+
+查看服务端存储的代码:http://tlmc.top:8090/ipget/07/code.lua.txt
---
+++ mote6_13/MyText.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+using XLua;
+
+public class MyText : MonoBehaviour
+{
+    [Hotfix]
+    Text myTtext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        load();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void load() {
+        LuaEnv luaEnv = new LuaEnv();
+
+        luaEnv.AddLoader(myLoader);
+
+        Debug.Log("start");
+        luaEnv.DoString("require 'byfile'");
+        string a = luaEnv.Global.Get<string>("text");
+        luaEnv.Dispose();
+        myTtext = this.GetComponent<Text>();
+        myTtext.text = a;
+    }
+    private byte[] myLoader( ref string url)
+    {
+        string url2 = "http://tlmc.top:8090/ipget/07/code.lua.txt";
+        //StartCoroutine(IECode(url2));
+        WebRequest request = WebRequest.Create(url2);
+        // If required by the server, set the credentials.
+        request.Credentials = CredentialCache.DefaultCredentials;
+        // Get the response.
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        // Display the status.
+        //Console.WriteLine(response.StatusDescription);
+        // Get the stream containing content returned by the server.
+        Stream dataStream = response.GetResponseStream();
+        // Open the stream using a StreamReader for easy access.
+        StreamReader reader = new StreamReader(dataStream);
+        // Read the content.
+        string responseFromServer = reader.ReadToEnd();
+        byte[] code = System.Text.Encoding.UTF8.GetBytes(responseFromServer);
+
+        Debug.Log(code);
+        return code;
+    }
+
+
+    
+}
